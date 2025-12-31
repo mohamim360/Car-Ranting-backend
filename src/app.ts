03504bb1481bc59ef3f6e457601ca134bfe129cd
mohamim360 <mohamim360@gmail.com>
2025-12-31 19:02:57 +0600

    chore (CORS) : update CORS configuration to allow all origins for simplified access

--- src/app.ts
+++ src/app.ts
@@ -8,16 +8,10 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "https://car-renting-backend-3.onrender.com"
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "*",
   })
 );
+
 
 app.use(express.json());
 app.use(cookieParser());
